803bdaed8b0447f2e86d16c515d7d6930908db4a
Gabriel F. (Frihet) <synapsedevbr@gmail.com>
2018-06-06 22:41:43 -0300

    Alteração da capitalização dos nomes das tabelas
    Alterada a capitalização dos nomes das tabelas para upper camel case (ou PascalCase).

--- database.sql
+++ database.sql
@@ -1,8 +1,8 @@
-DROP TABLE IF EXISTS `produto`;
+DROP TABLE IF EXISTS `Produto`;
 
 /* Categoria */
-DROP TABLE IF EXISTS `categoria`;
-CREATE TABLE `categoria` (
+DROP TABLE IF EXISTS `Categoria`;
+CREATE TABLE `Categoria` (
   `id` int(11) NOT NULL,
   `nome` tinytext NOT NULL,
   PRIMARY KEY (`id`)
@@ -10,7 +10,7 @@ CREATE TABLE `categoria` (
 /* Categoria */
 
 /* Produto */
-CREATE TABLE `produto` (
+CREATE TABLE `Produto` (
   `id` int(11) NOT NULL,
   `nome` tinytext NOT NULL,
   `preco` float NOT NULL,
@@ -19,13 +19,13 @@ CREATE TABLE `produto` (
   `categoria_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `categoria_id` (`categoria_id`),
-  FOREIGN KEY (`categoria_id`) REFERENCES `categoria` (`id`)
+  FOREIGN KEY (`categoria_id`) REFERENCES `Categoria` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /* Produto */
 
 /* Usuário */
-DROP TABLE IF EXISTS `usuario`;
-CREATE TABLE `usuario` (
+DROP TABLE IF EXISTS `Usuario`;
+CREATE TABLE `Usuario` (
   `login` varchar(255) NOT NULL,
   `senha` varchar(1024) NOT NULL,
   PRIMARY KEY (`login`)
