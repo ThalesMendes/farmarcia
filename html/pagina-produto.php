bfa4c0c72e0d632b7bdd16574ae62f3b7b425312
Gabriel Oliveira <gabriel_oliveira@terra.com.br>
2018-06-18 21:39:23 -0300

    Correção de erro do título da página individual de produtos
    Corrigido o erro em que o título da página individual de
     produtos ficava incompleto quando o produto não era encontrado ('Farmárcia - '). Agora, o título é exibido como 'Farmárcia - Produto não
    encontrado' nesse caso.

--- html/pagina-produto.php
+++ html/pagina-produto.php
@@ -10,7 +10,7 @@
 <head>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Farmárcia - <?= $product['nome']; ?></title>
+  <title>Farmárcia - <?= $product_text; ?></title>
 
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB"
     crossorigin="anonymous">
@@ -47,7 +47,7 @@
 
     <?php else: ?>
 
-      <p class="nenhum-produto">Produto não encontrado!</p>
+      <p class="nenhum-produto"><?= $product_text; ?></p>
 
     <?php endif; ?>
   </main>
