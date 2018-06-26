9830306b8ef0ea67bde49f2a84f0d597353f2db5
Gabriel Oliveira <gabriel_oliveira@terra.com.br>
2018-06-26 16:25:50 -0300

    Modificação do comportamento dos pop-ups de produto quando se aperta o botão voltar
    Quando se aperta voltar, o pop-up é fechado. Isso é importante, especialmente para dispositivos móveis.

--- assets/javascript/dialogo_produto.js
+++ assets/javascript/dialogo_produto.js
@@ -1,3 +1,12 @@
 $('#modal-produto').on('show.bs.modal', function (e) {
-    $(this).find('.modal-content').load(e.relatedTarget.getAttribute('dialogo-src'));
+  $(this).find('.modal-content').load(e.relatedTarget.getAttribute('dialogo-src'));
+});
+
+$(".modal").on("shown.bs.modal", function() {
+  var urlReplace = "#" + $(this).attr('id');
+  history.pushState(null, null, urlReplace);
+});
+
+$(window).on('popstate', function() {
+  $(".modal").modal('hide');
 });
